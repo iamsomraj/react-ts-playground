5fe46ddec5bbadcfe1a7ad21ea9fab60d3b9e7c1
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-15 21:05:46 +0530

    added usememo

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,3 +1,22 @@
+import { useMemo, useState } from 'react';
+
+const fib = (n: number): number => {
+  if (n <= 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
+
 export default function HelloWorld() {
-  return <div>Hello World</div>;
+  const [name, setName] = useState('');
+  const hugeCalculation = useMemo(() => fib(35), []);
+  return (
+    <div>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      {hugeCalculation}
+    </div>
+  );
 }
