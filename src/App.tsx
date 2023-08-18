7693edf312ff222c67073278598241b7e258eec6
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-18 11:26:30 +0530

    added timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import HelloWorld from './components/HelloWorld';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+  const intervalId = useRef<number | null>(null);
+
+  const onStart = () => {
+    if (intervalId.current) return;
+
+    intervalId.current = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const onStop = () => {
+    if (!intervalId.current) return;
+
+    clearInterval(intervalId.current);
+    intervalId.current = null;
+  };
   return (
     <>
-      <HelloWorld />
+      <div>{count}</div>
+      <div>
+        <button onClick={onStart}>Start</button>
+      </div>
+      <div>
+        <button onClick={onStop}>Stop</button>
+      </div>
     </>
   );
 }
