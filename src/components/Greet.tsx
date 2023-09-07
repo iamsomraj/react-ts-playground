b2fa6695a6fc0999a91733bcd90824dd29d1bf89
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-07 12:58:34 +0530

    added type checking

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -6,11 +6,15 @@ export type ToggleType = {
 
 const Greet = forwardRef<ToggleType>((_props, parentRef) => {
   const [show, setShow] = useState(false);
-  useImperativeHandle(parentRef, () => ({
-    toggle: () => {
-      setShow((prev) => !prev);
-    },
-  }));
+  useImperativeHandle(
+    parentRef,
+    () =>
+      ({
+        toggle: () => {
+          setShow((prev) => !prev);
+        },
+      } as ToggleType)
+  );
   return <div>{show ? 'Hey there!' : 'Sleeping'}</div>;
 });
 export default Greet;
