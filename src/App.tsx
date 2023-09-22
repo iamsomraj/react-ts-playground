b0b27cae5d4732dba7faf8fd6441c24859eba10c
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-22 20:53:59 +0530

    added coderbyte assessment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import HelloWorld from './components/HelloWorld';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    let intervalRef: number | undefined;
+    if (isRunning) {
+      intervalRef = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalRef);
+    }
+    return () => clearInterval(intervalRef);
+  }, [isRunning]);
+
+  const onCounterToggle = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const onCounterReset = () => {
+    setIsRunning(false);
+    setCounter(0);
+  };
+
   return (
-    <>
-      <HelloWorld />
-    </>
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={onCounterToggle}>{isRunning ? 'Pause' : 'Play'}</button>
+      <button onClick={onCounterReset}>Reset</button>
+    </div>
   );
 }
 
